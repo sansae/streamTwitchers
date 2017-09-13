c77c4db8508dbea41809d7a3adeb8ecb2c31d8d9
Kent Saeteurn <kentsaeteurn@gmail.com>
2017-09-13 00:58:14 -0700

    implement twitch api; first attempt with working demo; so far so good

--- script.js
+++ script.js
@@ -1,24 +1,98 @@
-// https://wind-bow.glitch.me/twitch-api
+/*use this api url to find out if twitch user is offline or not; if offline, we should see "stream: null"; if not, we'll see all the other data*/
+// https://wind-bow.glitch.me/twitch-api/streams/freecodecamp
+
+/*this api url gives general info about twitch user regardless of their online status*/
+//https://wind-bow.glitch.me/twitch-api/channels/freecodecamp
+
+/*to account for streamers that are offline, do the following:*/
+//first, make api call to "streams" api
+//then, inside success, do
+//if (stream value === null) {
+//  $("#status").html("offline");
+//} else {
+//  make api call to "channels" api
+//}
+
+$("button").click(function(){
+  $.ajax({
+    url: 'https://wind-bow.glitch.me/twitch-api/channels/freecodecamp',
+    success: function(data) {
+      console.log(JSON.stringify(data, undefined, 2));
+      var name = data.display_name
+      var game = data.game;
+      var status = data.status;
+      var logo = data.logo;
+      var url = data.url;
+      $("#name").html("<a target='_blank' href=" + url + ">" + name + "</a>");
+      $("#status").html(game + ": " + status);
+      var img = new Image();
+      img.src = logo;
+      $("#logo").html(img);
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      alert("Status: " + textStatus); alert("Error: " + errorThrown);
+    }
+  });
+})
+
+
+
+
+
+/***********************/
+/*ajax call below works*/
+/***********************/
+// $("button").click(function(){
+//   $.ajax({
+//     type: 'GET',
+//     url: 'https://api.twitch.tv/kraken/channels/freecodecamp',
+//     headers: {
+//       'Client-ID': 'axjhfp777tflhy0yjb5sftsil'
+//     },
+//     success: function(data) {
+//       console.log(JSON.stringify(data, undefined, 2));
+//       var name = data.display_name
+//       var status = data.status;
+//       var logo = data.logo;
+//       var url = data.url;
+//       $("#name").html("<a target='_blank' href=" + url + ">" + name + "</a>");
+//       $("#status").html(status);
+//       var img = new Image();
+//       img.src = logo;
+//       $("#logo").html(img);
+//     },
+//     error: function(XMLHttpRequest, textStatus, errorThrown) {
+//       alert("Status: " + textStatus); alert("Error: " + errorThrown);
+//     }
+//   });
+// })
+
+
+
+
+
 
 function startTime() {
   var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  var amPm = h >= 12 ? "PM" : "AM";
-  if (h === 0) {
-    h = 12;
-  } else if (h > 12) {
-    h = h % 12;
+  var hour = today.getHours();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
+  minute = checkTime(minute);
+  second = checkTime(second);
+  var amPm = hour >= 12 ? "PM" : "AM";
+  if (hour === 0) {
+    hour = 12;
+  } else if (hour > 12) {
+    hour = hour % 12;
   }
-  document.getElementById('txt').innerHTML = displayDate() + " -- " + h + ":" + m + ":" + s + " " + amPm + " PT";
+  document.getElementById('txt').innerHTML = displayDate() + " -- " + hour + ":" + minute + ":" + second + " " + amPm + " PT";
   var t = setTimeout(startTime, 500);
 }
 
 function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  if (i < 10) {
+    i = "0" + i;
+  }
   return i;
 }
 
